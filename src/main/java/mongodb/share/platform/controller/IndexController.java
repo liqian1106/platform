2bf51a0967e4a56307b2ef5c00ae1607418a1a5c
liqian1106 <1822545251@qq.com>
2023-03-14 15:38:40 +0800

    add index and bootstrap

--- src/main/java/mongodb/share/platform/controller/IndexController.java
+++ src/main/java/mongodb/share/platform/controller/IndexController.java
@@ -4,15 +4,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
 @Controller
-public class HelloController {
-    @GetMapping("/hello")
-    public String hello(@RequestParam(name = "name")String name, Model model) {
-        model.addAttribute("name",name);
-        return "hello";
+public class IndexController {
+    @GetMapping("/")
+    public String index(){return "index";}
 
     }
 
-}
+
